a4209732e808c559b6ad3a0554a612f91e27f500
JOSEFYAACOUBB <155582334+JOSEFYAACOUBB@users.noreply.github.com>
2025-04-25 11:44:50 +0100

    Update delete_tourist.php

--- delete_tourist.php
+++ delete_tourist.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../includes/config.php';
+require_once __DIR__ . 'config.php';
 
 if (!is_logged_in() || !is_admin()) {
     $_SESSION['error_message'] = 'Accès non autorisé';
